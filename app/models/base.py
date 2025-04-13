b8c793df29b403e1be748e9cfe5ba8e0e05f4f3f
RPleshkov <iamromanpleshkov@gmail.com>
2025-04-13 23:09:22 +0300

    create base model

---
+++ app/models/base.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+
+from app.core.config import settings
+from sqlalchemy import DateTime, MetaData
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql import func
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.postgres.naming_convention)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    updated_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True), onupdate=func.now()
+    )
