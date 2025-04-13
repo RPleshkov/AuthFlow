829334be1d58c6b83519c2ca7403fe7c1f4b8103
RPleshkov <iamromanpleshkov@gmail.com>
2025-04-13 22:57:41 +0300

    init main.py

---
+++ app/main.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.routing import APIRoute
+
+from app.api import api_router
+from app.core.config import settings
+
+
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
+
+
+app = FastAPI(
+    title=settings.project_name,
+    generate_unique_id_function=custom_generate_unique_id,
+)
+
+
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
+app.include_router(api_router, prefix=settings.api_v1_str)
