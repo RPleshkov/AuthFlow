dfe1e09d5894fb1052a18af7b6a7ba5ef69d4cc6
RPleshkov <iamromanpleshkov@gmail.com>
2025-04-14 23:34:33 +0300

    update crud

--- app/database/crud.py
+++ app/database/crud.py
@@ -1,16 +1,16 @@
 from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.security import hash_password
+from app.core.security import hash_password, validate_password
 from app.models import User
-from app.schemas.user import UserCreate
+from app.schemas import UserCreate
 
 
 async def create_user(session: AsyncSession, user_create: UserCreate) -> User:
-    user = User(
-        email=user_create.email,
-        password=hash_password(user_create.password),
-    )
+    params = user_create.model_dump()
+    params["password"] = hash_password(params["password"])
+    user = User(**params)
+
     session.add(user)
     await session.commit()
     return user
@@ -20,6 +20,15 @@ async def get_user_by_email(session: AsyncSession, email: str) -> User | None:
     stmt = select(User).where(User.email == email)
     user = await session.execute(stmt)
     return user.scalar_one_or_none()
+
+
+async def authenticate(session: AsyncSession, email: str, password: str) -> User | None:
+    user = await get_user_by_email(session=session, email=email)
+    if not user:
+        return None
+    if not validate_password(password, user.password):
+        return None
+    return user
 
 
 async def confirm_user(session: AsyncSession, email: str) -> None:
