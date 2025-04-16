36499c07c1cd67b017acc3e45b97b3f024d17d5b
RPleshkov <iamromanpleshkov@gmail.com>
2025-04-16 22:39:16 +0300

    add http baerer depend

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.api.routes.auth import router as auth_router
+from fastapi.security import HTTPBearer
+
+http_bearer = HTTPBearer(auto_error=False)
 
 __all__ = ("api_router",)
 
 
-api_router = APIRouter()
+api_router = APIRouter(
+    dependencies=[Depends(http_bearer)],
+)
 api_router.include_router(auth_router)
