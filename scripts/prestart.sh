c0ea4bbefa6097b2ad8a11dabe95073e3dbe5b7e
RPleshkov <iamromanpleshkov@gmail.com>
2025-04-17 22:54:37 +0300

    modify script

--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -16,10 +16,17 @@ PUBLIC_KEY="app/core/certs/public_key.pem"
 
 
 if [ ! -f "$PRIVATE_KEY" ]; then
+    echo "Generating private key..."
     openssl genrsa -out "$PRIVATE_KEY" 2048
+else
+    echo "Private key already exists."
 fi
-
 
 if [ ! -f "$PUBLIC_KEY" ]; then
+    echo "Generating public key..."
     openssl rsa -in "$PRIVATE_KEY" -outform PEM -pubout -out "$PUBLIC_KEY"
+else
+    echo "Public key already exists."
 fi
+
+exec "$@"
